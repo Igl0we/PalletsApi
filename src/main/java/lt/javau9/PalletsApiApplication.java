6585f980d49d901f55c76258a314a2f118de300f
Kestutis <kestas.plok@gmail.com>
2024-04-04 15:56:02 +0300

    Spring boot + dependecies

---
+++ src/main/java/lt/javau9/PalletsApiApplication.java
@@ -0,0 +1,13 @@
+package lt.javau9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalletsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalletsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/javau9/palletsApi/PalletsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.javau9.palletsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalletsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
